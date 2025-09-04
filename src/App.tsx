40c2cd9faf2613c985123ce7e479d4ced7b46785
Reece <reecemorgandev@gmail.com>
2025-09-04 10:36:47 +0100

    add header and footer to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import { Box } from "@mui/material";
 import logo from "./assets/logo.svg";
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
 
 const App = () => {
   return (
-    <Box
-      sx={{
-        padding: "2rem 0 0 0",
-        height: "100%",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
+    <>
+      <Header />
       <Box
-        component="img"
-        src={logo}
-        alt="Allus Interactive Logo"
-        sx={{ width: "100%", height: "auto", minWidth: "300px" }}
-      />
-      <h2>New Site Coming Soon!</h2>
-    </Box>
+        sx={{
+          padding: "2rem 0 0 0",
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          maxWidth: "1280px",
+          margin: "0 auto",
+        }}
+      >
+        <Box
+          component="img"
+          src={logo}
+          alt="Allus Interactive Logo"
+          sx={{ width: "400px", height: "auto", minWidth: "300px" }}
+        />
+        <br />
+        <h2>New Site Coming Soon!</h2>
+      </Box>
+      <Footer />
+    </>
   );
 };
 
