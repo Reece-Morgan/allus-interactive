8a91d4f1fa461f3b7dba676a95b0443e957293f8
Reece <reecemorgandev@gmail.com>
2025-09-10 11:18:33 +0100

    updated formatting

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,20 +1,16 @@
-export const aboutText = `
-Hi! I'm Reece, and I run Allus Interactive!
+export const aboutText = `Hi! I'm Reece, and I run Allus Interactive!
 
 Allus Interactive is an indie game development studio who loves to design and build small fun games for people to play.
 
 Founded in 2018, we have a developed and released a differing style of games whilst taking part in virtual and in-person game jams. These games range from small arcade-style games, to little prototypes to larger, more narrative-driven RPG games.
 `;
 
-export const mgjText = `
-The Moray Game Jam is an annual event that brings together game developers from around Moray, Scotland (and beyond!) to create games in a 48 hour period.
+export const mgjText = `The Moray Game Jam is an annual event that brings together game developers from around Moray, Scotland (and beyond!) to create games in a 48 hour period.
 
 Allus Interactive has been proud to be a part of the Moray Game Jam for the last several years, and we look forward to participating in many more events in the future.
 `;
 
-
-export const placeholderText = `
-Prow scuttle parrel provost Sail ho shrouds spirits boom mizzenmast yardarm. Pinnace holystone mizzenmast quarter crow's nest nipperkin grog yardarm hempen halter furl. Swab barque interloper chantey doubloon starboard grog black jack gangway rutters.
+export const placeholderText = `Prow scuttle parrel provost Sail ho shrouds spirits boom mizzenmast yardarm. Pinnace holystone mizzenmast quarter crow's nest nipperkin grog yardarm hempen halter furl. Swab barque interloper chantey doubloon starboard grog black jack gangway rutters.
 
 Deadlights jack lad schooner scallywag dance the hempen jig carouser broadside cable strike colors. Bring a spring upon her cable holystone blow the man down spanker Shiver me timbers to go on account lookout wherry doubloon chase. Belay yo-ho-ho keelhaul squiffy black spot yardarm spyglass sheet transom heave to.
 
